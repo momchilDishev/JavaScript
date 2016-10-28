062d790546860a807ed0fb36220bf22660a368cb
MoGD <mo.dishev@gmail.com>
2016-10-28 17:05:11 +0300

    homeworks moved to another folder

---
+++ Homeworks/01.Homework-JavaScript-Introduction/03.current-time.js
@@ -0,0 +1,9 @@
+﻿var time = new Date();
+var hours = time.getHours();
+var minutes = time.getMinutes();
+if (minutes < 10) {
+    console.log(hours + ":0" + minutes);
+}
+else {
+    console.log(hours + ":" + minutes);
+}
---
+++ Homeworks/01.Homework-JavaScript-Introduction/04.circle-area.js
@@ -0,0 +1,6 @@
+﻿var r;
+function calcCircleArea(r) {
+    r = document.input.radius.value;
+    document.write("<P>r = " + r + "; area = " + (r * r * Math.PI) + "</p>");
+
+}
